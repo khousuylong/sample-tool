d067f7a4cb1babf4ee5a168534f90f128dd1ac47
suylong <khou.suylong@gmail.com>
2020-10-22 10:52:02 +0700

    added json-server

--- project/src/schema/resolvers.js
+++ project/src/schema/resolvers.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
+const server = "http://localhost:4006"
 
 const resolvers = {
   Query: {
       pluginSetting: async (root, args, context, info) => {
-        const pluginSetting = (await axios.get(`http://localhost:3004/pluginSettings/${args.id}`)).data;
+        const pluginSetting = (await axios.get(`${server}/pluginSettings/${args.id}`)).data;
         return pluginSetting;
       }
   },
   Mutation: {
     updatePluginSetting: async (root, args, context, info) => {
-      const pluginSetting = (await axios.get(`http://localhost:3004/pluginSettings/${args.id}`)).data;
+      const pluginSetting = (await axios.get(`${server}/pluginSettings/${args.id}`)).data;
       pluginSetting['setting'] = args.setting;
-      return (await axios.put(`http://localhost:3004/pluginSettings/${args.id}`, pluginSetting, { headers: {"Content-Type": "application/json"}})).data;
+      return (await axios.put(`${server}/pluginSettings/${args.id}`, pluginSetting, { headers: {"Content-Type": "application/json"}})).data;
     }
   }
 };
